bcf2468b25c2052ef2d0d930cd96322b3c3393ac
Julian Alihuen Viola <gotrealit@gmail.com>
2022-02-14 17:19:12 -0300

    Modify Routes and Add index Page

---
+++ resources/views/indexApi.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Consume API - Json Placeholder Photos</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+
+    <body>
+        <div>
+            <h1>
+                Consume API - Json Placeholder Photos
+            </h1>
+            <a href="/api/photos/download">Download Photos from JsonPlaceHolder</a><br><br>
+            <a href="/api/photos">See the Photos saved in the Local Database</a>
+            <br>
+            
+            
+        </div>
+
+    </body>
+
+</html>
