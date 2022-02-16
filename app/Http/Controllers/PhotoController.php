a61d038ce81c2e6b5c614a5febda4798edffd164
Julian Alihuen Viola <gotrealit@gmail.com>
2022-02-16 02:11:07 -0300

    Update PhotoController.php

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Photo;
-use Illuminate\Http\Request;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Http\Client\Pool;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\JsonResponse;
 
+ini_set('max_execution_time', 360);
 
 class PhotoController extends Controller
 {
@@ -16,7 +16,7 @@ class PhotoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(): JsonResponse
     {
         $photos = Photo::all();
 
@@ -26,24 +26,59 @@ class PhotoController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return JSON Response 
      */
-    public function store(Request $request)
+    public function store(): JsonResponse
     {
-        $responses = Http::acceptJson()->get('https://jsonplaceholder.typicode.com/photos/');
-        $photos = $responses->json();
-
-        foreach ($photos as $photo)
-        { 
-            $arrayPhoto = array('album_id' => $photo['albumId'], 'id' => $photo['id'],
-                'title' => $photo['title'], 'url' => $photo['url'],
-                'thumbnail_url' => $photo['thumbnailUrl']);
-
-            Photo::create($arrayPhoto);
-            echo "Created Photo:" . $photo['id'] ."<br>";   
-        }
+        $response = Http::retry(10, 100)->acceptJson()->get('https://jsonplaceholder.typicode.com/photos/');
         
-        return "<br>Finished";
+        if($response->successful())
+        {
+            $photos = $response->json();
+            
+            foreach ($photos as $photo)
+            { 
+                $validator = $this->validatePhoto($photo);
+
+                if($validator->fails())
+                {
+                    return response()->json(["message" => $validator->messages()])
+                                            ->setStatusCode(400);
+                }
+                
+                $arrayPhoto = array('album_id' => $photo['albumId'], 'id' => $photo['id'],
+                     'title' => $photo['title'], 'url' => $photo['url'],
+                     'thumbnail_url' => $photo['thumbnailUrl']);
+                
+                Photo::create($arrayPhoto);
+            }
+
+            return response()->json(['message' => 'OK', "status" => $response->status()])
+                            ->setStatusCode($response->status());
+        }
+        else
+        {
+            return response()->json(['message' => 'ERROR', "status" => $response->status()])
+                            ->setStatusCode($response->status());
+        }
+    }
+    
+    /**
+     * Validate input data
+     * 
+     * @param  Array $photo
+     * @return Object $validator
+     */
+    protected function validatePhoto(Array $photo): Object
+    {
+        $validator = Validator::make($photo, [
+                    'albumId' => 'required|integer',
+                    'id' => 'required|integer',
+                    'title' => 'required|string|max:255',
+                    'url' => 'required|string|max:255',
+                    'thumbnailUrl' => 'required|string|max:255',
+                ]);
+        
+        return $validator;
     }
 }
